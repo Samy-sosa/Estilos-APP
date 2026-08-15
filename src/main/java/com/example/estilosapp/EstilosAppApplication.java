5db3e7afe165b4bf98e93a97b50f985c4a1105b6
Samy-sosa <samysosa069@gmail.com>
2026-08-15 10:04:27 -0600

    feat:Creacion del proyecto

---
+++ src/main/java/com/example/estilosapp/EstilosAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.estilosapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstilosAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EstilosAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/estilosapp/EstilosAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.estilosapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstilosAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
